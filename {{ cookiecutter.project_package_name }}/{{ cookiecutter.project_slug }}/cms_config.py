d8d4a7f6e98a8d9cd00cc05222086d44a82f33f5
Evgeny Dmitriev <evgeny.dmi3ev@gmail.com>
2022-06-28 14:49:34 +0200

    fix import

--- {{ cookiecutter.project_package_name }}/{{ cookiecutter.project_slug }}/cms_config.py
+++ {{ cookiecutter.project_package_name }}/{{ cookiecutter.project_slug }}/cms_config.py
@@ -6,7 +6,6 @@ from cms.utils.i18n import get_language_tuple
 from djangocms_versioning.datastructures import VersionableItem, default_copy
 from djangocms_versioning.admin import ExtendedVersionAdminMixin
 
-from {{ cookiecutter.project_slug }}.admin import ChangeLinkesAdminMixin
 from {{ cookiecutter.project_slug }}.models import {{ cookiecutter.project_class }}Content
 from {{ cookiecutter.project_slug }}.rendering import render_{{ cookiecutter.project_class.lower() }}_content
 
